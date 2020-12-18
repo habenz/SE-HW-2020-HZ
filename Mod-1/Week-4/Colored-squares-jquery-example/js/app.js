c66720bf95c768230e719d19d9d1cb1c22b9cf0e
Haben Zaid <haben.zaidt@gmail.com>
2020-12-18 00:46:40 -0500

    Added basic folder structure and colored squares class exercise to mod 1 week 4

---
+++ Mod-1/Week-4/Colored-squares-jquery-example/js/app.js
@@ -0,0 +1,21 @@
+const randColorRGB = () => {
+	const red = Math.floor(Math.random() *256);
+	const green = Math.floor(Math.random() *256);
+	const blue = Math.floor(Math.random() *256);
+	return `rgb(${red},${green},${blue})`;
+}
+
+const generateSquares = (numSquares) => {
+	for (let i =1;i<=numSquares; i++){
+		const $square = $("<div>").addClass("square");
+		$square.css("background-color",randColorRGB());
+		$square.text(i);
+		$square.attr("id","square"+i);
+		$("body").append($square);
+	}
+}
+
+$(()=> {
+	generateSquares(1000);
+})
+
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Homework repo for Per Scholas Software Engineering class 2020
 
 Repo Structure should be:
 
-- Mod 1
-  - Week 1
-  - Week 2
-  - Week 3
-  - Week 4
-- Mod 2
+- Mod-1
+  - Week-1
+  - Week-2
+  - Week-3
+  - Week-4
+- Mod-2
