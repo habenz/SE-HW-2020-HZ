1f56761e8d3c5e4cf06d4257728324daec91a956
Haben Zaid <haben.zaidt@gmail.com>
2021-01-15 14:24:44 -0500

    added styling, first taco now shows up automatically

--- Mod-2/Week-1/w1d4-lab-Random-Taco/app.js
+++ Mod-2/Week-1/w1d4-lab-Random-Taco/app.js
@@ -2,31 +2,37 @@
 class App extends React.Component {
 	state = {
 		taco: '',
+		loaded: false
+	}
+	
+	componentDidMount() {
+		this.handleNewTaco();	
 	}
 
 	handleNewTaco = () => {
 		const URL = 'http://taco-randomizer.herokuapp.com/random/';
 		fetch(URL)
 			.then((response) => {
-				// this function call needed to pass along json? Not sure
+				// this function call needed to pass along json? Not sure why exactly
 				return response.json();
 			})
-			// .then((taco) => this.setState({taco: taco}))
 			.then((taco) =>{
 				console.log(taco)
 				console.log(taco.shell.name)
 				return this.setState({
-					taco: taco
+					taco: taco,
+					loaded: true
 				});
 			})
 
 	}
+
 	render() {
 		return (
 			<div >
-				<h1> Random Taco </h1>
-				{!(this.state.taco === '') ? <Taco data={this.state.taco}/> : null}
-				<button onClick={this.handleNewTaco}> New Taco!</button>
+				<h1 className="main-title"> Random Taco! </h1>
+				<button className="random-button" onClick={this.handleNewTaco}> New Taco!</button>
+				{(this.state.loaded) ? <Taco data={this.state.taco}/> : null}
 			</div>
 			)
 	}
@@ -35,8 +41,8 @@ class App extends React.Component {
 
 class Taco extends React.Component {
 	render() {
+		// names of the taco portions we want to display
 		const components = ['shell', 'mixin', 'condiment']
-		console.log(this.props.data[components[0]]);
 		return(
 			<div >
 				{components.map((comp)=> <TitledParagraph title={comp} content={this.props.data[comp]}/>)}
@@ -45,6 +51,7 @@ class Taco extends React.Component {
 	}
 }
 
+// helper function for display
 function makeCapital(word){
 	return word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase();
 }
@@ -53,11 +60,11 @@ class TitledParagraph extends React.Component {
 	render() {
 		return (
 			<div >
-				<h3>
-					<span>{makeCapital(this.props.title)}:</span> 
+				<h2 className="paragraph-title">
+					<span className="paragraph-type">{makeCapital(this.props.title)}: </span> 
 					{this.props.content.name}
-				</h3>
-				<p>{this.props.content.recipe}</p>
+				</h2>
+				<p><pre>{this.props.content.recipe}</pre></p>
 			</div>
 			)
 	}
