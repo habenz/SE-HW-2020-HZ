18eac2b0e7f6f3f027b910d8b869c6cfa094566c
Haben Zaid <haben.zaidt@gmail.com>
2021-02-15 22:37:44 -0500

    Finished HW

--- Mod-3/Mod3_Week1_HW2/src/mod3_week1_hw2/CollectionManipulation.java
+++ Mod-3/Mod3_Week1_HW2/src/mod3_week1_hw2/CollectionManipulation.java
@@ -1,16 +1,106 @@
 package mod3_week1_hw2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionManipulation {
 	
+	// similar to ArrayList clone
 	public static <T> List<T> copyList(List<T> list){
-		// could I just use ArrayList clone?
 		List<T> copy = new ArrayList<T>();
 		for(T item: list) {
 			copy.add(item);
 		}
 		return copy;
 	}
+	
+	// similar to List subList
+	public static <T> List<T> sliceList(List<T> list, int fromIndex){
+		List<T> slice = new ArrayList<T>();
+		if (fromIndex >= list.size()) {
+			return slice;
+		}
+		
+		for(int i= fromIndex; i<list.size(); i++) {
+			slice.add(list.get(i));
+		}
+		return slice;	
+	}
+	
+	public static <T> List<T> sliceList(List<T> list, int fromIndex, int toIndex){
+		List<T> slice = new ArrayList<T>();
+		if (fromIndex >= list.size() || fromIndex > toIndex) {
+			return slice;
+		}
+		
+		for(int i= fromIndex; i<list.size() && i<toIndex; i++) {
+			slice.add(list.get(i));
+		}
+		return slice;	
+	}
+	
+	//	Write a Java program of swap two elements in an array list
+	public static <T> void swap(List<T> list, int index1, int index2) {
+		if (index1 < list.size() && index2 < list.size()) {
+			T temp = list.get(index1);
+			list.set(index1, list.get(index2));
+			list.set(index2, temp);
+		}
+	}
+	
+	//	Write a Java program to test an array list is empty or not
+	public static <T> boolean isEmpty(List<T> list) {
+		return list.size() == 0;
+	}
+		
+	//	Write a Java program to replace the second element of a ArrayList with the specified element.
+	public static <T> void replaceSecondWith(List<T> list, T replacement) {
+		if (list.size()>=2) {
+			list.set(1, replacement);
+		}
+	}
+	
+	//	Write a Java program to trim the capacity of an array list the current list size
+	// ArrayList has trimToSize()
+	public static <T> void trimCapacity(ArrayList<T> list) {
+		list.trimToSize();
+	}	
+	
+	//	 Write a Java program to test a hash set is empty or not.
+	public static <T> boolean isEmpty(Set<T> set) {
+		return set.isEmpty();
+	}
+	
+	//	Write a Java program to get the number of elements in a hash set
+	public static <T> int getSetSize(Set<T> set) {
+		return set.size();
+	}
+	//	Write a Java program to iterate through all elements in a hash list.
+	public static <T> void printAllElements(Set<T> set) {
+		for (T element: set) {
+			System.out.println("Set Element: "+element);
+		}
+	}
+	
+	//	Write a Java program to convert a hash set to an array.
+	// can't return T[] https://www.baeldung.com/java-generic-array
+	public static <T> Object[] toPrimitiveArray(Set<T> set) {
+		return set.toArray();
+	}
+	
+	//	Write a Java program to compare two sets and retain elements which are same on both sets.
+	// set1.retainAll(set2) will make set1 be the intersection of the sets, below is a non mutating method
+	public static <T> Set<T> intersect(Set<T> set1, Set<T> set2) {
+		Set<T> intersect = new HashSet<T>();
+		for (T element : set1) {
+			if (set2.contains(element)) {
+				intersect.add(element);
+			}
+		}
+		
+		return intersect;
+	}
+	
 }
--- Mod-3/Mod3_Week1_HW2/src/mod3_week1_hw2/Main.java
+++ Mod-3/Mod3_Week1_HW2/src/mod3_week1_hw2/Main.java
@@ -1,13 +1,43 @@
 package mod3_week1_hw2;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
-
+// Example uses for most methods. Examples of trimToSize and toPrimitiveArray omitted
+	
 	public static void main(String[] args) {
-		ArrayList<String> stringList = new ArrayList<String>(
-				List.of("Hello", "World", "I'm", "exploring", "collections"));
-		System.out.println(CollectionManipulation.copyList(stringList));
+		List<String> stringList = new ArrayList<String>(
+				List.of("Hello", "world", "I'm", "exploring", "collections"));
+		
+		System.out.println("copied: "+CollectionManipulation.copyList(stringList));
+		System.out.println("sliced (2,3): "+ CollectionManipulation.sliceList(stringList, 2, 3));
+		CollectionManipulation.swap(stringList, 1, 4);
+		System.out.println("swapped (1,4): "+ stringList);
+		System.out.println("is stringList empty? "+ CollectionManipulation.isEmpty(stringList));
+		
+		List<Integer> fibList = Arrays.asList(1, 1, 2,3,5,8);
+		CollectionManipulation.replaceSecondWith(fibList, 100);
+		System.out.println("replaced second element: "+fibList);
+		
+		Set<Double> harmonic = new HashSet<Double>();
+		System.out.println("is set empty? "+ CollectionManipulation.isEmpty(harmonic));
+		
+		harmonic.addAll(List.of(1.0, 0.5, 0.33, 0.25, 0.2, 0.1667));
+		System.out.println("set is of size: "+ CollectionManipulation.getSetSize(harmonic));
+		System.out.println("printing out set...");
+		CollectionManipulation.printAllElements(harmonic);
+		
+		Set<Double> halvingFromTwo = new HashSet<Double>();
+		halvingFromTwo.addAll(List.of(2.0, 1.0, 0.5, 0.25, 0.125));
+		
+		System.out.println("harmonic instersect halvingFromTwo: "+ 
+				CollectionManipulation.intersect(harmonic, halvingFromTwo));
+		
+		
 	}
 
 }
